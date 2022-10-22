27aa1eb84cb42dd118edceaf1917140e17332f2a
pj <sangvongp@carleton.edu>
2022-10-22 17:11:38 -0500

    Add timeSeriesService

---
+++ WikiExtension/src/enums.js
@@ -0,0 +1,9 @@
+// Values used for timeSeriesService
+const WIKI_CREATION_DATE = new Date("2001-01-15"); // 15 January 2001: to fetch data since creation
+
+const AggregateType = {
+    MONTHLY: "monthly",
+    DAILY: "daily",
+};
+
+export { WIKI_CREATION_DATE, AggregateType };
