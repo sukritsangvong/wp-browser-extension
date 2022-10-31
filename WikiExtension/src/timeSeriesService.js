da55699da1f9e5fe070af14e6088af3489dd06c3
pj <sangvongp@carleton.edu>
2022-10-30 19:19:23 -0500

    Add revision count endpoint

--- WikiExtension/src/timeSeriesService.js
+++ WikiExtension/src/timeSeriesService.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import { AggregateType } from "./enums.js";
 
 const formatDateToYYYMMDD = (date) =>
     `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`;
@@ -11,10 +12,10 @@ const formatYYYYMMDDToDate = (yyyymmdd) =>
  * if the startDate comes before the article was created.
  *
  * @param {string} title of a wikipedia article
- * @param {Date} startTimestamp in a format of YYYYMMDD
- * @param {Date} endDate in a format of YYYYMMDD
+ * @param {Date} startDate
+ * @param {Date} endDate
  * @param {string} aggregateType of either monthly or daily
- * @return {map} of an item of list with properties project, article, granularity(aggregationType), timestamp, access, agent, views
+ * @return {map} of an item of list with properties project, article, granularity(aggregateType), timestamp, access, agent, views
  */
 const fetchPageViews = async (title, startDate, endDate, aggregateType) => {
     const formattedStartDate = formatDateToYYYMMDD(startDate);
@@ -26,6 +27,52 @@ const fetchPageViews = async (title, startDate, endDate, aggregateType) => {
 };
 
 /**
+ * Get all the revision objects on a given wikipedia title.
+ *
+ * The api only give us at most 20 reivision objects per request. However, it does give us a query to
+ * use if there are any older revisions. If older versions of revision existm, we will get
+ * {
+ *  revisions: [...],
+ *  ...
+ *  older: 'https://en.wikipedia.org/w/rest.php/v1/page/Jupiter/history?older_than=1103899458',
+ *  ...
+ * }
+ *
+ * Thus, we keep calling the endpoint with the given querys if the older property exists in the
+ * previous request.
+ *
+ * @param {string} title of a wikipedia article
+ * @param {Date} startDate
+ * @param {Date} endDate
+ * @returns An array containing all revision objects
+ */
+const fetchPageRevisions = async (title, startDate, endDate) => {
+    const response = await fetch(`https://en.wikipedia.org/w/rest.php/v1/page/${title}/history`);
+    let curJsonResponse = await response.json();
+
+    let revisions = curJsonResponse.revisions;
+
+    while (curJsonResponse.hasOwnProperty("older")) {
+        const localResponse = await fetch(curJsonResponse.older);
+        curJsonResponse = await localResponse.json();
+
+        const localRevisions = curJsonResponse.revisions;
+
+        // Stop making requests when requesting passes startDate
+        const localLatestTimestamp = new Date(localRevisions[0].timestamp);
+        if (localLatestTimestamp < startDate) break;
+
+        revisions.push(...localRevisions);
+    }
+
+    // Filters revisions that are in between the given timerange.
+    return revisions.filter((revisionObject) => {
+        const currentTimestamp = new Date(revisionObject.timestamp);
+        return startDate <= currentTimestamp && currentTimestamp <= endDate;
+    });
+};
+
+/**
  * TODO: Subject to change based on what the graph needs
  * @return formated fetchedPageViews as a list of lists that only contain date object and count
  */
@@ -33,6 +80,30 @@ const formatPageViews = (fetchedPageViews) => {
     return Array.from(fetchedPageViews.items).map((pageViewObject) => {
         return [formatYYYYMMDDToDate(pageViewObject.timestamp), pageViewObject.views];
     });
+};
+
+/**
+ * TODO: Subject to change based on what the graph needs
+ * @param {AggregateType} aggregateType
+ * @return formated fetchedRevisions as a list of lists that only contain date object and count
+ */
+const formatPageRevisions = (fetchedRevisions, aggregateType) => {
+    return fetchedRevisions
+        .map((revisionObject) => {
+            const date = new Date(revisionObject.timestamp);
+            date.setHours(0, 0, 0, 0); // have date only contain year, month, and day (not time)
+
+            // Reduce dates to 1 to aggregate monthly instead of daily
+            if (aggregateType == AggregateType.MONTHLY) {
+                date.setDate(1);
+            }
+
+            return date;
+        })
+        .reduce((accumulator, timestamp) => {
+            accumulator[timestamp] = (accumulator[timestamp] ?? 0) + 1;
+            return accumulator;
+        }, {});
 };
 
 /**
@@ -53,8 +124,22 @@ const getPageViews = async (title, startDate, endDate, aggregateType) => {
         console.error(
             `Error fetching page views on inputs title:${title} startDate:${startDate} endDate:${endDate} aggregateType:${aggregateType}\nError: ${err.message}`
         );
-        return null;
+        return err;
     }
 };
 
-export default getPageViews;
+const getPageRevisionCount = async (title, startDate, endDate, aggregateType) => {
+    try {
+        let result = await fetchPageRevisions(title, startDate, endDate);
+        let formattedResult = formatPageRevisions(result, aggregateType);
+        return formattedResult;
+    } catch (err) {
+        console.error(
+            `Error fetching revision count on inputs title:${title} startDate:${startDate} endDate:${endDate} aggregateType:${aggregateType}\nError: ${err.message}`
+        );
+
+        return err;
+    }
+};
+
+export { getPageViews, getPageRevisionCount };
