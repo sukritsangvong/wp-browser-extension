37ca215a5b810ca53641489c3d1283f32271c1d8
LuisaE <luisaescosteguy@hotmail.com>
2023-01-11 10:16:40 -0600

    let to const

--- wikichange/src/popup/popup.js
+++ wikichange/src/popup/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let compsLink = document.getElementById("comps-website");
+    const compsLink = document.getElementById("comps-website");
     compsLink.onclick = function () {
         chrome.tabs.create({active: true, url: compsLink.href});
     };
