7e56dacf29031c37b6963e70818aa6b7e4c5a45b
Shirin <>
2023-01-17 13:16:03 -0600

    updatedGraph

--- wikichange/src/content/graph.js
+++ wikichange/src/content/graph.js
@@ -1,4 +1,5 @@
 import Chart from "chart.js/auto";
+import { Tooltip } from 'chart.js';
 import { getPageViewTimeseries, getPageRevisionCountTimeseries } from "./timeSeriesService.js";
 
 const CHART_COLORS = {
@@ -9,6 +10,7 @@ const CHART_COLORS = {
     blue: "rgb(54, 162, 235)",
     purple: "rgb(153, 102, 255)",
     grey: "rgb(201, 203, 207)",
+    dgrey: "rgb(220, 200, 200)",
 };
 
 /**
@@ -17,7 +19,18 @@ const CHART_COLORS = {
  * @param {string} title of the article
  * @param {Date} startDate
  * @param {Date} endDate
+ * Tooltip.positioners.myCustomPositioner = function(elements, eventPosition) {
+    // A reference to the tooltip model
+    const tooltip = this;
+
+
+    return {
+        x: 0,
+        y: 0
+    };
+};
  */
+ 
 const injectGraphToPage = async (title, startDate, endDate) => {
     const pageViews = await getPageViewTimeseries(title, startDate, endDate);
     const revisions = await getPageRevisionCountTimeseries(title, startDate, endDate);
@@ -30,16 +43,20 @@ const injectGraphToPage = async (title, startDate, endDate) => {
             {
                 label: "Views",
                 data: pageViews["y"],
-                borderColor: CHART_COLORS.red,
+                borderColor: CHART_COLORS.grey,
                 yAxisID: "y",
+                fillStyle: CHART_COLORS.grey,
                 borderWidth: 2,
+                lineWidth: 4,
             },
             {
                 label: "Edits",
                 data: revisions["y"],
                 borderColor: CHART_COLORS.blue,
+                fillStyle: CHART_COLORS.blue,
                 yAxisID: "y1",
                 borderWidth: 2,
+                lineWidth: 4,
             },
         ],
     };
@@ -48,6 +65,11 @@ const injectGraphToPage = async (title, startDate, endDate) => {
         type: "line",
         data: data,
         options: {
+            plugins: {
+                tooltip: {
+                    position: 'nearest'
+                }
+            },
             scales: {
                 y: {
                     type: "linear",
