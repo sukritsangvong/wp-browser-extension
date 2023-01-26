996ffa23021824708bcd2e1a3a44199f597229a2
pj <sangvongp@carleton.edu>
2023-01-25 21:49:28 -0600

    Fix date.

--- wikichange/src/content/enums.js
+++ wikichange/src/content/enums.js
@@ -1,6 +1,6 @@
 // Values used for timeSeriesService
 const WIKI_CREATION_DATE = new Date("2001-01-15"); // 15 January 2001: to fetch data since creation
-const WIKI_PAGE_VIEW_DATA_AVAILABLE_DATE = new Date("2015-12-01");
+const WIKI_PAGE_VIEW_DATA_AVAILABLE_DATE = new Date("2015-08-01");
 
 const AggregateType = {
     MONTHLY: "monthly",
