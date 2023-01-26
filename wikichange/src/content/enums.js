1832fea7c8260dc8d35ca9d9e3ab2d8be9668fd1
PJ <55929961+sukritsangvong@users.noreply.github.com>
2023-01-25 22:03:29 -0600

    Fix continuous dot graph (#48)

--- wikichange/src/content/enums.js
+++ wikichange/src/content/enums.js
@@ -1,9 +1,10 @@
 // Values used for timeSeriesService
 const WIKI_CREATION_DATE = new Date("2001-01-15"); // 15 January 2001: to fetch data since creation
+const WIKI_PAGE_VIEW_DATA_AVAILABLE_DATE = new Date("2015-08-01");
 
 const AggregateType = {
     MONTHLY: "monthly",
     DAILY: "daily",
 };
 
-export { WIKI_CREATION_DATE, AggregateType };
+export { WIKI_CREATION_DATE, WIKI_PAGE_VIEW_DATA_AVAILABLE_DATE, AggregateType };
