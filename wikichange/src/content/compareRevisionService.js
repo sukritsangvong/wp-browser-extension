69d3ca47a96c2da7d34b376368fc1af153af721d
PJ <55929961+sukritsangvong@users.noreply.github.com>
2023-02-05 12:53:10 -0600

    Combine contents that are mergable (#66)

--- wikichange/src/content/compareRevisionService.js
+++ wikichange/src/content/compareRevisionService.js
@@ -21,8 +21,9 @@ const fetchChangeWithHTML = async (startID, endID) => {
         (v, i, a) => a.findIndex((v2) => v2.innerHTML === v.innerHTML) === i
     );
 
-    const result = [];
+    let result = [];
     divsWithIns.forEach((element) => {
+        const localResult = [];
         element.childNodes.forEach((child, i) => {
             const nodeName = child.nodeName;
             const content = child.textContent;
@@ -35,14 +36,41 @@ const fetchChangeWithHTML = async (startID, endID) => {
                         ? element.childNodes[i + 1].textContent
                         : "";
 
-                addJsonToResult(result, contentBefore, content, contentAfter);
+                addJsonToResult(localResult, contentBefore, content, contentAfter);
             }
         });
+
+        const combinedResult = [];
+        localResult.forEach((child) => {
+            if (
+                combinedResult.length != 0 &&
+                isOnlyContainsSymbols(child.content_before) &&
+                combinedResult.at(-1).content_after == child.content_before
+            ) {
+                const previouslyAddedResult = combinedResult.at(-1);
+                previouslyAddedResult.highlight =
+                    previouslyAddedResult.highlight + previouslyAddedResult.content_after + child.highlight;
+                previouslyAddedResult.content_after = child.content_after;
+            } else {
+                combinedResult.push(child);
+            }
+        });
+        result.push(...combinedResult);
     });
 
     divsWithNoInsOuts.forEach((curDiv) => {
         addJsonToResult(result, "", curDiv.innerText, "");
     });
+
+    result = result
+        .map((res) => {
+            return {
+                content_before: cleanUpContent(res.content_before),
+                highlight: cleanUpContent(res.highlight),
+                content_after: cleanUpContent(res.content_after),
+            };
+        })
+        .filter((res) => !isOnlyContainsSymbols(res.highlight) && !res.highlight == "");
     return result;
 };
 
@@ -51,20 +79,19 @@ const fetchChangeWithHTML = async (startID, endID) => {
  * @returns cleaned up content that has no {{...}}, <ref>...</ref>, <ref>..., and ...</ref>
  */
 const cleanUpContent = (content) => {
-    return content.replace(/{{.*?}}|<ref.*?<\/ref>/g, "").replace(/<ref>.*$|.*$<\/ref>/g, "");
+    return content.replace(/{{.*?}}|<ref.*?<\/ref>/g, "").replace(/<ref.*|.*<\/ref>/g, "");
 };
 
 const isOnlyContainsSymbols = (text) => /^[\W\s]+$/.test(text);
 
 const addJsonToResult = (result, contentBefore, highlight, contentAfter) => {
     // don't add to json if highlight is empty or only contains symbols
-    const cleanUpHighlight = cleanUpContent(highlight);
-    if (cleanUpHighlight == "" || isOnlyContainsSymbols(cleanUpHighlight)) return;
+    if (highlight == "" || isOnlyContainsSymbols(highlight)) return;
 
     result.push({
-        content_before: cleanUpContent(contentBefore),
-        highlight: cleanUpHighlight,
-        content_after: cleanUpContent(contentAfter),
+        content_before: contentBefore,
+        highlight: highlight,
+        content_after: contentAfter,
     });
 };
 
