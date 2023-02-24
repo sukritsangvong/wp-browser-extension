d84b7351ec3c3374af2760159f709f89a2ef8a33
pj <sangvongp@carleton.edu>
2023-02-23 19:22:28 -0600

    Add restore button

--- wikichange/src/options/options.js
+++ wikichange/src/options/options.js
@@ -2,17 +2,20 @@
 
 /** Saves options to chrome.storage */
 function save_options() {
-    const color = document.getElementById('highlight-color').value;
-    chrome.storage.sync.set({
-        highlightColor: color
-    }, function () {
-        /** Update status to let user know options were saved. */
-        const status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
-    });
+    const color = document.getElementById("highlight-color").value;
+    chrome.storage.sync.set(
+        {
+            highlightColor: color,
+        },
+        function () {
+            /** Update status to let user know options were saved. */
+            const status = document.getElementById("status");
+            status.textContent = "Options saved.";
+            setTimeout(function () {
+                status.textContent = "";
+            }, 750);
+        }
+    );
 }
 
 /** Restores select box and checkbox state using the preferences
@@ -20,16 +23,22 @@ function save_options() {
  */
 function restore_options() {
     /** Use default value highlightColor #AFE1AF */
-    chrome.storage.sync.get({
-        highlightColor: '#AFE1AF'
-    }, function (items) {
-        console.log(items);
-        document.getElementById('highlight-color').value = items.highlightColor;
-    });
+    chrome.storage.sync.get(
+        {
+            highlightColor: "#AFE1AF",
+        },
+        function (items) {
+            document.getElementById("highlight-color").value = items.highlightColor;
+        }
+    );
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('form').addEventListener('submit', event => {
+document.addEventListener("DOMContentLoaded", restore_options);
+document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
     save_options();
-})
+});
+console.log(document.getElementById("restore-color"));
+document.getElementById("restore-color").addEventListener("click", () => {
+    document.getElementById("highlight-color").value = "#AFE1AF";
+});
